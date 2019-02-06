0a6d0750766a1e6b645382e1599a664806e44940
saravana <saravanasaru14313@gmail.com>
2019-02-06 02:56:41 -0800

    Update README.java

--- README.java
+++ README.java
@@ -1,12 +1,12 @@
 import java.io.*;
 import java.util.*;
-class NumLine
+class Numcount
 {
 public static void main(String args[])
 	{
 	Scanner input=new Scanner(System.in);
 	String a=input.nextLine();
-	int count=0;
+	int count=1;
 	for(int i=0;i<a.length();i++)
 	{
 	    if(a.charAt(i)=='.')
